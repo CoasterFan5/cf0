42c717cb5e009dbfae8001d80ff8eb4289bbb2da
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-04-25 16:19:03 -0400

    add files this time

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,3 @@
+export const load = () => {
+	return { r: Math.random() * 10 };
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+
+	const {
+		data
+	}: {
+		data: PageServerData;
+	} = $props();
+</script>
+
+<h1>CF0</h1>
+<p>Front</p>
+<p>Back: {data.r}</p>
